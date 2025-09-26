683ceb25fc92c5f99c87d223fe4aa28a00023afd
Hashaam Zafar <hashaam1217@gmail.com>
2025-09-26 15:22:31 -0700

    Question 3 Complete

--- Assignment_2/Hashaam_Solution_Q3.m
+++ Assignment_2/Hashaam_Solution_Q3.m
@@ -4,6 +4,5 @@
 
 
 Q3.Tp = 0.367;
-Q3.wnp = 20;
-Q3.G = tf(1, [1, 2 , 1]); 
-a2Submit
+Q3.wnp = pi / (Q3.Tp * sqrt(1 - Q2.zeta^2));
+Q3.G = tf(Kdc * Q3.wnp^2, [1, 2 * Q2.zeta * Q3.wnp , Q3.wnp^2]); 
