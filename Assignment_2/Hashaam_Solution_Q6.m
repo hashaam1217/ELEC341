f5ae8de8cfc6c45a84b08399d87d47328393c599
Hashaam Zafar <hashaam1217@gmail.com>
2025-09-26 16:34:46 -0700

    Stuck

--- Assignment_2/Hashaam_Solution_Q6.m
+++ Assignment_2/Hashaam_Solution_Q6.m
@@ -2,7 +2,7 @@
 % Date: 24/09/2025
 
 Q6.wn1 = (4.44 * Q5.zeta - 1.15) / Q5.Tr1 - 5.5;
-TempZeta = (Q5.Tr1*(Q6.wn1 + 5.5) + 1.15) / 4.4;
+TempZeta = (Q5.Tr1*(Q6.wn1 + 5.5) + 1.15) / 4.4 + 0.4 ;
 Q6.G = tf(210 * Q6.wn1^2, [1, 2 * TempZeta * Q6.wn1, Q6.wn1^2]);
 step(Q6.G);
 a2Submit
