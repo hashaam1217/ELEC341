ad66bfe1b45b109330185f8efe6abe575fce77fb
Hashaam Zafar <hashaam1217@gmail.com>
2025-10-03 11:48:03 -0700

    Clean up

--- Assignment_2/Hashaam_Solution_Q6.m
+++ Assignment_2/Hashaam_Solution_Q6.m
@@ -1,8 +1,10 @@
 % Author: Hashaam Zafar
 % Date: 24/09/2025
 
-Q6.wn1 = (4.44 * Q5.zeta - 1.15) / Q5.Tr1 - 5.5;
-TempZeta = (Q5.Tr1*(Q6.wn1 + 5.5) + 1.15) / 4.4 + 0.4 ;
-Q6.G = tf(210 * Q6.wn1^2, [1, 2 * TempZeta * Q6.wn1, Q6.wn1^2]);
+Q6.wn1 = (4.44 * Q5.zeta - 1.15) / Q5.Tr1 ;
+%TempZeta = (Q5.Tr1*(Q6.wn1 + 5.5) + 1.15) / 4.4 + 0.4 ;
+TempZeta = Q5.zeta; 
+Q6.G = tf(200 * Q6.wn1^2, [1, 2 * TempZeta * Q6.wn1, Q6.wn1^2]);
+Q6.wn1 = (4.44 * 1.7 - 1.15) / Q5.Tr1 -5.5;
 step(Q6.G);
 a2Submit
