9fa33f74ab2293d5d5549eb153cccbb6d3a43c97
Hashaam Zafar <hashaam1217@gmail.com>
2025-10-16 18:38:31 -0700

    Assignment 5 init

---
+++ Assignment_5/Assignment_5.m
@@ -0,0 +1,17 @@
+% Name: Hashaam Zafar
+% Date: 16/10/2025
+% Assignment 5
+
+% Setting up student number variables
+clear all; clc; 
+SN = 10078020;
+A = 11;
+B = 10; 
+C = 10; 
+D = 17; 
+E = 18;
+F = 10; 
+G = 12; 
+H = 10; 
+
+% Figure 1 Variables
