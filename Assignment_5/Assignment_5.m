a400bfe61fb44bffe5cd2ca7ccac0c543172e6a9
Hashaam Zafar <hashaam1217@gmail.com>
2025-10-17 15:00:58 -0700

    Assignment 5 buffer

--- Assignment_5/Assignment_5.m
+++ Assignment_5/Assignment_5.m
@@ -15,3 +15,20 @@ G = 12;
 H = 10; 
 
 % Figure 1 Variables
+Js = A * 20 * 1e-6; % uNms^2
+n = 5; % rev / m
+Mn = B / 2; % Kg
+Bn = C / 2; % Ns/m
+
+% Assignment 3: Figure 1 Variables
+M0 = A / 5; % Kg
+M1 = B / 10; % Kg
+M2 = C / 10; % Kg
+M3 = D / 5; % Kg
+B20 = E / 2; % Ns/m
+B21 = F / 3; % Ns/m
+B31 = G / 4; % Ns/m
+K0 = A; % N/m
+K1 = B; % N/m
+K20 = C; % N/m
+K32 = D / 3; % N/m
