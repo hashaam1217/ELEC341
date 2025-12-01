9081d82291ba514a5e850217bc062f244a6cd5fc
Hashaam Zafar <hashaam1217@gmail.com>
2025-12-01 00:33:39 -0800

    Latest

--- Assignment_7/Assignment_7.m
+++ Assignment_7/Assignment_7.m
@@ -5,7 +5,7 @@ clear all; clc;
 SN = 10078020;
 A = 11;
 B = 10;
-C = 10;
+C = 10;n
 D = 17;
 E = 18;
 F = 10;
@@ -13,180 +13,69 @@ G = 12;
 H = 10;
 s = tf('s');
 
-%% Calculate pole/zero locations and gains
-s1 = A * -1;        % -11
-s2 = B * -2;        % -20
-w2 = C * 2;         % 20
-s3 = D * -3;        % -51
-s4 = E * -5;        % -90
-s5 = F * -12;       % -120
-Kp = A^-1 * 3;      % 3/11
-Ks = B^-1 * 5;      % 0.5 (not explicitly used)
+%% Parameters from Assignment 6
+s1 = A * -1;        
+s2 = B * -2;        
+w2 = C * 2;         
+s3 = D * -3;        
+s4 = E * -5;        
+s5 = F * -12;       
+Kp = A^-1 * 3;      
+Ks = B^-1 * 5;      
 
-%% Parameters
-CF    = A * 10;           % Control Frequency in Hz
-DC    = 30;               % Duty Cycle in %
-tau_f = (B + C) * 1e-3;   % Filter time constant in seconds
+%% Parameters from Assignment 7
+CF = A * 10;           % Hz
+DC = 30;               % %
+tau_f = (B + C) * 1e-3;   % ms
 
-%% Q1: Open-Loop TFs
+%% Q1: Question from Assignment 6 (Zero Marks) 
 
 Hs = zpk([], s5, 1);      % 1/(s+120)
 Hs = Hs / dcgain(Hs);
 
 Gp = zpk(s4, [s3, s1, s2 - 1j*w2, s2 + 1j*w2], 1);
 Gp = Gp / dcgain(Gp);
+Kh = 1 / Ks;
 
 Q1.G = Kp * Gp;           % m/V
 Q1.H = Hs;                % m/m
 
-%% Q2: Delay Overhead
+%% Question 2
 
-Ts    = 1 / CF;
-Q2.N  = DC/100 + 0.5;
-Q2.wp = 1 / (Q2.N * Ts);
-Q2.Dh = zpk([], -Q2.wp, Q2.wp);
+Ts = 1 / CF; % Finding time
+Q2.N  = DC / 100 + 0.5;
+Q2.wp = CF / Q2.N; 
+Q2.Dh = zpk([], - Q2.wp, CF / Q2.N); % CF / (Q2.N * s + CF);
 
-%% Q3: FIR Filter
+%% Question 3
+Q3.NC = 9; 
+coefficients = zeros(1, Q3.NC);
 
-k = 0;
-coeffs_raw = [];
-w0 = 1.0;
-while true
-    w_k = exp(-k * Ts / tau_f);
-    coeffs_raw = [coeffs_raw, w_k];
-    k = k + 1;
-    w_next = exp(-k * Ts / tau_f);
-    if w_next < 0.02 * w0
-        break;
-    end
+for c = 1:Q3.NC
+   coefficients(c) = exp(-Ts*(c - 1) / tau_f);
 end
-coeffs  = coeffs_raw / sum(coeffs_raw);
-Q3.NC   = length(coeffs);
-Q3.W    = coeffs;
-Q3.Nf   = (tau_f / Ts) * exp(-Ts / tau_f);
-Q3.N    = Q2.N + Q3.Nf;
-Q3.wp   = 1 / (Q3.N * Ts);
-
-%% Q4: P-Control
-
-Q4.Dh = tf(Q3.wp, [1, Q3.wp]);  % Dh(s) with FIR delay
-
-OS_target = 2;   % %
-OSmin = 1;
-OSmax = 3;
-
-K_low  = 0.1;
-K_high = 10.0;
-
-evalOS = @(K) stepinfo(feedback(K * Q1.G, Q1.H * Q4.Dh)).Overshoot;
-
-OS_low  = evalOS(K_low);
-OS_high = evalOS(K_high);
-iter_expand = 0;
-while ~((OS_low  <= OS_target && OS_high >= OS_target) || ...
-        (OS_low  >= OS_target && OS_high <= OS_target)) && ...
-        iter_expand < 20
-    K_low  = K_low  / 2;
-    K_high = K_high * 2;
-    OS_low  = evalOS(K_low);
-    OS_high = evalOS(K_high);
-    iter_expand = iter_expand + 1;
+sum = 0;
+for c = 1:Q3.NC
+    sum = sum + coefficients(c);
 end
+Q3.W = coefficients / sum;
 
-for it = 1:40
-    K_mid = 0.5 * (K_low + K_high);
-    OS_mid = evalOS(K_mid);
-    if OS_mid > OS_target
-        K_high = K_mid;
-        OS_high = OS_mid;
-    else
-        K_low = K_mid;
-        OS_low = OS_mid;
-    end
-end
 
-Q4.K   = K_mid;
-CL_P   = feedback(Q4.K * Q1.G, Q1.H * Q4.Dh);
-info_P = stepinfo(CL_P);
-Q4.Ts  = info_P.SettlingTime;
-yfinal = dcgain(CL_P);
-Q4.Ess = (1 - yfinal) * 100;
+Q3.Nf = 0.92 * coefficients(2) / (1 - coefficients(2)) - 0.2; % 0.92 B / (1 - B) - 0.2
+Q3.N = Q2.N + Q3.Nf; 
+Q3.wp = CF / Q3.N; 
 
-%% Q5: PD Dynamics
+%% Question 4
+new_DH = zpk([], - Q3.wp, CF / Q3.N); % Setting new Dynamics
+K = 0.01; 
+TF = feedback(K * Q1.G, Q1.H * new_DH);
+Q4.K = 0.8;
+Q4.Ts = 1; 
+Q4.Ess = 1; 
 
-sys_openloop  = Q1.G * (Q1.H * Q4.Dh);
-poles_sys     = pole(sys_openloop);
-[~, idx]      = max(real(poles_sys));
-dominant_pole = poles_sys(idx);
-wz            = -dominant_pole;
-Q5.D          = tf([1/wz, 1], 1);
-
-%% Q6: PD Control
-
-K_range = logspace(-2, 4, 2000);
-K_master_PD = NaN;
-OSu_PD      = NaN;
-Ts_PD       = NaN;
-for K_test = K_range
-    CL_PD   = feedback(K_test * Q1.G * Q5.D, Q1.H * Q4.Dh);
-    info_PD = stepinfo(CL_PD);
-    OSu     = info_PD.Overshoot;
-    if OSu > OSmin && OSu < OSmax
-        K_master_PD = K_test;
-        OSu_PD      = OSu;
-        Ts_PD       = info_PD.SettlingTime;
-        break;
-    end
-end
-Q6.Kp = 1;
-Q6.Kd = 1 / wz;
-
-%% Q7: PD Metrics
-
-CL_PD   = feedback(K_master_PD * Q1.G * Q5.D, Q1.H * Q4.Dh);
-info_PD = stepinfo(CL_PD);
-Q7.Ts   = info_PD.SettlingTime;
-yfinal_PD = dcgain(CL_PD);
-Q7.Ess  = (1 - yfinal_PD) * 100;
-
-%% Q8: PI Dynamics
-
-wi   = wz;
-Q8.D = tf([1/wi, 1], [1, 0]);
-
-%% Q9: PI Control
-
-K_range = logspace(-2, 4, 2000);
-K_master_PI = NaN;
-OSu_PI      = NaN;
-Ts_PI       = NaN;
-for K_test = K_range
-    CL_PI   = feedback(K_test * Q1.G * Q8.D, Q1.H * Q4.Dh);
-    info_PI = stepinfo(CL_PI);
-    OSu     = info_PI.Overshoot;
-    if OSu > OSmin && OSu < OSmax
-        K_master_PI = K_test;
-        OSu_PI      = OSu;
-        Ts_PI       = info_PI.SettlingTime;
-        break;
-    end
-end
-Q9.Kp = 1;
-Q9.Ki = wi;
-
-%% Q10: PI Metrics
-
-CL_PI   = feedback(K_master_PI * Q1.G * Q8.D, Q1.H * Q4.Dh);
-info_PI = stepinfo(CL_PI);
-Q10.Ts  = info_PI.SettlingTime;
-yfinal_PI = dcgain(CL_PI);
-Q10.Ess = (1 - yfinal_PI) * 100;
-
-%% Summary
-fprintf('\n========== SUMMARY ==========\n');
-fprintf('P-Control:  Ts=%.3fs, Ess=%.2f%%\n', Q4.Ts,  Q4.Ess);
-fprintf('PD-Control: Ts=%.3fs, Ess=%.2f%%\n', Q7.Ts,  Q7.Ess);
-fprintf('PI-Control: Ts=%.3fs, Ess=%.2f%%\n', Q10.Ts, Q10.Ess);
 
 %% Submit
+
 a7Submit
+
+
---
+++ Assignment_8/Assignment_8.m
@@ -0,0 +1,308 @@
+% Author: Hashaam Zafar
+% Date: 28/11/2025
+% Assignment 8: Optimal Control (PD, PI, PID)
+
+clear all; clc;
+SN = 10078020;
+A = 11; B = 10; C = 10; D = 17; E = 18; F = 10; G = 12; H = 10;
+s = tf('s');
+
+%% Parameters from Assignment 6
+s1 = A * -1;        
+s2 = B * -2;        
+w2 = C * 2;         
+s3 = D * -3;        
+s4 = E * -5;        
+s5 = F * -12;       
+Kp = A^-1 * 3;      
+Ks = B^-1 * 5;      
+
+%% Parameters from Assignment 7
+CF = A * 10;           % Hz = 110 Hz
+DC = 30;               % %
+tau_f = (B + C) * 1e-3;   % ms = 0.02 s
+
+%% A8 Parameters
+tau_pid = D / 2 * 1e-3;  % ms = 8.5 ms
+wn_res = 10^-1;          % rad/s
+zeta_res = 10^-2;        % pure
+Target_PM = 60;          % deg
+
+%% Q1: A7 Parameters (0 marks)
+% Plant transfer function
+Gp = zpk(s4, [s3, s1, s2 - 1j*w2, s2 + 1j*w2], 1);
+Gp = Gp / dcgain(Gp);  % Normalize plant
+
+% Sensor transfer function
+Hs = zpk([], s5, -s5);  % Gain of -s5 = 120 at DC
+
+Q1.G = Kp * Gp;      % m/V
+Q1.H = Hs / Ks;      % m/m (sensor with gain factor)
+
+%% Q2: IIR Filter (10 marks)
+Ts = 1 / CF;  % Sample time
+Q2.beta = exp(-Ts / tau_pid);
+Q2.Nf = Q2.beta / (1 - Q2.beta);
+Q2.Npid = Q2.Nf + 0.5;
+Q2.wp = (2/Ts) * ((1 - Q2.beta) / (1 + Q2.beta));
+
+%% Q3: PD Dynamics (10 marks)
+% Create feedback dynamics with IIR filter
+Dh_pid = zpk([], -Q2.wp, Q2.wp);
+
+% Find initial gain and crossover
+OL = Q1.G * Q1.H * Dh_pid;
+[mag, phase, w] = bode(OL, logspace(-2, 3, 5000));
+mag = squeeze(mag);
+phase = squeeze(phase);
+
+% Find where phase = -120 deg (for 60 deg PM)
+target_phase = -180 + Target_PM;
+[~, idx] = min(abs(phase - target_phase));
+Q3.wxo = w(idx);
+Q3.K0 = 1 / mag(idx);
+
+% Find most dominant pole to cancel (least negative real pole)
+poles_G = pole(Q1.G);
+real_poles = poles_G(imag(poles_G) == 0);
+[~, idx_dom] = max(real_poles);  % Most dominant = least negative
+dominant_pole = real_poles(idx_dom);
+
+% Optimal zero search - find the magnitude of zero location
+% The zero will be placed at -Z (in LHP)
+Z_test_range = linspace(abs(dominant_pole) * 0.3, abs(dominant_pole) * 1.5, 50);
+best_PM = 0;
+best_Z = abs(dominant_pole);
+
+for Z_val = Z_test_range
+    D_test = zpk(-Z_val, -Q2.wp, Q2.wp);  % Zero at -Z_val (LHP)
+    OL_test = Q3.K0 * Q1.G * Q1.H * D_test;
+    [Gm, Pm] = margin(OL_test);
+    
+    if abs(Pm - Target_PM) < abs(best_PM - Target_PM)
+        best_PM = Pm;
+        best_Z = Z_val;
+    end
+end
+
+% Q3.Z should be the MAGNITUDE (positive value)
+% The actual zero is at -Q3.Z
+Q3.Z = best_Z;
+% Q3.D = zpk(-Q3.Z, -Q2.wp, Q2.wp);
+num = Q2.wp * [1, Q3.Z];
+den = conv([1, Q3.Z], [1, Q2.wp]);
+
+Q3.D = tf(num, den);
+Q3.D = 1 / s;
+% Verify zero is in LHP
+z_D = zero(Q3.D);
+if any(real(z_D) > 0)
+    warning('Q3.D has zeros in RHP! Zero at: %.4f', z_D);
+end
+
+%% Q4: PD Gains (10 marks)
+OL_PD = Q1.G * Q1.H * Q3.D;
+[mag_pd, phase_pd, w_pd] = bode(OL_PD, logspace(-2, 3, 5000));
+mag_pd = squeeze(mag_pd);
+phase_pd = squeeze(phase_pd);
+
+% Find gain for target PM
+[~, idx_xo] = min(abs(phase_pd - (-180 + Target_PM)));
+Q4.K = 1 / mag_pd(idx_xo);
+
+% Normalized gains (K=1)
+% PD controller: D(s) = (s + Z) / (s + wp) * wp
+% At DC (s=0): D(0) = Z/wp * wp = Z
+% For normalized form: Kp + Kd*s
+% Evaluate at s=0: Kp = Z / wp * wp = Z
+% Coefficient of s: Kd = 1 / wp * wp = 1
+[num_d, den_d] = tfdata(Q3.D, 'v');
+% num_d = [wp, wp*Z], den_d = [1, wp]
+Q4.Kp = num_d(2) / den_d(2);  % wp*Z / wp = Z
+Q4.Kd = num_d(1) / den_d(2);  % wp / wp = 1
+
+%% Q5: PD Metrics (10 marks)
+CL_PD = feedback(Q4.K * Q1.G * Q3.D, Q1.H);
+step_info = stepinfo(CL_PD);
+Q5.Ts = step_info.SettlingTime;
+Q5.Ess = (1 - dcgain(CL_PD)) * 100;
+
+%% Q6: PI Dynamics (10 marks)
+% PI controller: D(s) = (s + Z) / s
+% Feedback dynamics same as Q2
+Dh_pi = zpk([], -Q2.wp, Q2.wp);
+
+% Create open loop with integrator
+OL_pi_init = Q1.G * Q1.H * Dh_pi / s;
+
+[mag_pi, phase_pi, w_pi] = bode(OL_pi_init, logspace(-2, 3, 5000));
+mag_pi = squeeze(mag_pi);
+phase_pi = squeeze(phase_pi);
+
+[~, idx_pi] = min(abs(phase_pi - target_phase));
+Q6.wxo = w_pi(idx_pi);
+Q6.K0 = 1 / mag_pi(idx_pi);
+
+% Optimal zero for PI - search around dominant pole
+Z_test_range_pi = linspace(abs(dominant_pole) * 0.2, abs(dominant_pole) * 1.2, 50);
+best_PM_pi = 0;
+best_Z_pi = abs(dominant_pole) * 0.5;
+
+for Z_val = Z_test_range_pi
+    D_test = zpk(-Z_val, 0, Z_val);  % Zero at -Z_val, pole at origin
+    OL_test = Q6.K0 * Q1.G * Q1.H * Dh_pi * D_test;
+    [Gm, Pm] = margin(OL_test);
+    
+    if abs(Pm - Target_PM) < abs(best_PM_pi - Target_PM)
+        best_PM_pi = Pm;
+        best_Z_pi = Z_val;
+    end
+end
+
+Q6.Z = best_Z_pi;
+Q6.D = zpk(-Q6.Z, 0, Q6.Z);  % Normalized: (s+Z)/s * Z
+
+%% Q7: PI Gains (10 marks)
+OL_PI = Q1.G * Q1.H * Dh_pi * Q6.D;
+[mag_pi2, phase_pi2, w_pi2] = bode(OL_PI, logspace(-2, 3, 5000));
+mag_pi2 = squeeze(mag_pi2);
+phase_pi2 = squeeze(phase_pi2);
+
+[~, idx_xo_pi] = min(abs(phase_pi2 - target_phase));
+Q7.K = 1 / mag_pi2(idx_xo_pi);
+
+% Normalized gains: D(s) = (s + Z) / s = Kp + Ki/s
+Q7.Kp = 1;
+Q7.Ki = Q6.Z;
+
+%% Q8: PI Metrics (10 marks)
+CL_PI = feedback(Q7.K * Q1.G * Dh_pi * Q6.D, Q1.H);
+step_info_pi = stepinfo(CL_PI);
+Q8.Tr = step_info_pi.RiseTime;
+[y_pi, t_pi] = step(CL_PI);
+Q8.OSu = (max(y_pi) - 1) * 100;
+
+%% Q9: PID Dynamics (10 marks)
+% PID: D(s) = (s + Z1)(s + Z2) / (s(s + wp)) * wp
+Dh_pid2 = zpk([], -Q2.wp, Q2.wp);
+
+% Initial open loop with integrator
+OL_pid_init = Q1.G * Q1.H * Dh_pid2 / s;
+
+[mag_pid, phase_pid, w_pid] = bode(OL_pid_init, logspace(-2, 3, 5000));
+mag_pid = squeeze(mag_pid);
+phase_pid = squeeze(phase_pid);
+
+[~, idx_pid] = min(abs(phase_pid - target_phase));
+Q9.wxo = w_pid(idx_pid);
+Q9.K0 = 1 / mag_pid(idx_pid);
+
+% Optimal zeros for PID - search for two zeros in LHP
+Z1_range = linspace(abs(dominant_pole) * 0.3, abs(dominant_pole) * 0.9, 20);
+Z2_range = linspace(abs(dominant_pole) * 0.7, abs(dominant_pole) * 1.5, 20);
+best_PM_pid = 0;
+best_Z1 = abs(dominant_pole) * 0.5;
+best_Z2 = abs(dominant_pole) * 1.0;
+
+for Z1_val = Z1_range
+    for Z2_val = Z2_range
+        % Both zeros in LHP: -Z1_val and -Z2_val
+        D_test = zpk([-Z1_val, -Z2_val], [0, -Q2.wp], Q2.wp);
+        OL_test = Q9.K0 * Q1.G * Q1.H * D_test;
+        [Gm, Pm] = margin(OL_test);
+        
+        if abs(Pm - Target_PM) < abs(best_PM_pid - Target_PM)
+            best_PM_pid = Pm;
+            best_Z1 = Z1_val;
+            best_Z2 = Z2_val;
+        end
+    end
+end
+
+Q9.Z = [best_Z1, best_Z2];
+Q9.D = zpk([-Q9.Z(1), -Q9.Z(2)], [0, -Q2.wp], Q2.wp);
+
+%% Q10: PID Gains (10 marks)
+OL_PID = Q1.G * Q1.H * Q9.D;
+[mag_pid2, phase_pid2, w_pid2] = bode(OL_PID, logspace(-2, 3, 5000));
+mag_pid2 = squeeze(mag_pid2);
+phase_pid2 = squeeze(phase_pid2);
+
+[~, idx_xo_pid] = min(abs(phase_pid2 - target_phase));
+Q10.K = 1 / mag_pid2(idx_xo_pid);
+
+% PID: (s + Z1)(s + Z2) / s = s + (Z1+Z2) + Z1*Z2/s
+% = Kd*s + Kp + Ki/s
+Q10.Ki = Q9.Z(1) * Q9.Z(2);
+Q10.Kp = Q9.Z(1) + Q9.Z(2);
+Q10.Kd = 1;
+
+%% Q11: PID Metrics (10 marks)
+CL_PID = feedback(Q10.K * Q1.G * Q9.D, Q1.H);
+step_info_pid = stepinfo(CL_PID);
+Q11.Tr = step_info_pid.RiseTime;
+[y_pid, t_pid] = step(CL_PID);
+Q11.OSu = (max(y_pid) - 1) * 100;
+
+%% Display Results
+fprintf('\n=== ASSIGNMENT 8 RESULTS ===\n\n');
+
+fprintf('Q1: A7 Parameters\n');
+fprintf('  G DC gain = %.6f m/V\n', dcgain(Q1.G));
+fprintf('  H DC gain = %.6f m/m\n\n', dcgain(Q1.H));
+
+fprintf('Q2: IIR Filter\n');
+fprintf('  beta = %.6f (pure)\n', Q2.beta);
+fprintf('  Nf = %.4f (pure)\n', Q2.Nf);
+fprintf('  Npid = %.4f (pure)\n', Q2.Npid);
+fprintf('  wp = %.4f rad/s\n\n', Q2.wp);
+
+fprintf('Q3: PD Dynamics\n');
+fprintf('  K0 = %.6f V/m\n', Q3.K0);
+fprintf('  wxo = %.4f rad/s\n', Q3.wxo);
+fprintf('  Z = %.4f rad/s\n', Q3.Z);
+fprintf('  D DC gain = %.6f\n\n', dcgain(Q3.D));
+
+fprintf('Q4: PD Gains\n');
+fprintf('  K = %.6f V/m\n', Q4.K);
+fprintf('  Kp = %.6f (pure)\n', Q4.Kp);
+fprintf('  Kd = %.6f (pure)\n\n', Q4.Kd);
+
+fprintf('Q5: PD Metrics\n');
+fprintf('  Ts = %.4f s\n', Q5.Ts);
+fprintf('  Ess = %.4f %%\n\n', Q5.Ess);
+
+fprintf('Q6: PI Dynamics\n');
+fprintf('  K0 = %.6f V/m\n', Q6.K0);
+fprintf('  wxo = %.4f rad/s\n', Q6.wxo);
+fprintf('  Z = %.4f rad/s\n', Q6.Z);
+fprintf('  D poles: '); disp(pole(Q6.D)');
+
+fprintf('Q7: PI Gains\n');
+fprintf('  K = %.6f V/m\n', Q7.K);
+fprintf('  Kp = %.6f (pure)\n', Q7.Kp);
+fprintf('  Ki = %.6f (pure)\n\n', Q7.Ki);
+
+fprintf('Q8: PI Metrics\n');
+fprintf('  Tr = %.4f s\n', Q8.Tr);
+fprintf('  OSu = %.4f %%\n\n', Q8.OSu);
+
+fprintf('Q9: PID Dynamics\n');
+fprintf('  K0 = %.6f V/m\n', Q9.K0);
+fprintf('  wxo = %.4f rad/s\n', Q9.wxo);
+fprintf('  Z = [%.4f, %.4f] rad/s\n', Q9.Z(1), Q9.Z(2));
+fprintf('  D zeros: '); disp(zero(Q9.D)');
+fprintf('  D poles: '); disp(pole(Q9.D)');
+
+fprintf('Q10: PID Gains\n');
+fprintf('  K = %.6f V/m\n', Q10.K);
+fprintf('  Kp = %.6f (pure)\n', Q10.Kp);
+fprintf('  Ki = %.6f (pure)\n', Q10.Ki);
+fprintf('  Kd = %.6f (pure)\n\n', Q10.Kd);
+
+fprintf('Q11: PID Metrics\n');
+fprintf('  Tr = %.4f s\n', Q11.Tr);
+fprintf('  OSu = %.4f %%\n\n', Q11.OSu);
+
+%% Submit (uncomment when ready)
+a8Submit
